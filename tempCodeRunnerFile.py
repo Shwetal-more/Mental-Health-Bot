76ba3ca6bc22e1769a0ecd2bcff8221aac1e6c99
shubhangi24kashid <kashidshubhangi24@gmail.com>
2023-11-08 13:42:12 +0530

    i have started to ask question to user and taking responses

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,5 +1,5 @@
 response=0
-res1=input("How have you been feeling lately (Rate in range of 0 to 100%)?")
+res1=int(input("How have you been feeling lately (Rate in range of 0 to 100%)?"))
 if res1>0 and res1<100:
     print(response + res1)
 else:
