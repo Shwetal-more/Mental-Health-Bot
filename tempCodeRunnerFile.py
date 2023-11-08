ebd1e6f8819903fa32d60c1bddb731b10c5f6d45
shubhangi24kashid <kashidshubhangi24@gmail.com>
2023-11-08 14:00:25 +0530

    Started question forming

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,9 +1,27 @@
 response=0
-res1=int(input("How have you been feeling lately (Rate in range of 0 to 100%)?"))
-if res1>0 and res1<100:
-    print(response + res1)
-else:
-    print("Please give answer in correct range")
- 
 
+while True:
+   res1=int(input("1. How have you been feeling lately (Rate in range of 0 to 100%)?"))
+   if res1>0 and res1<100:
+      response=response+ res1
+      break
+   else:
+      print("Please give answer in correct range")
+
+while True:
+  res2=int(input("2. Are there any specific situations or events causing you stress or anxiety(Rate in range of 0 to 100%)?"))
+  if res2>0 and res2<100:
+    response=response+ res2
+    break
+  else:
+    print("Please give answer in correct range")
+
+
+while True:
+    res3=int(input("3. On a scale of 0 to 100 , how would you rate your overall modd today?"))
+    if res3>0 and res3<100:
+      response=response+ res3
+      break
+    else:
+      print("Please give answer in correct range")    
 
