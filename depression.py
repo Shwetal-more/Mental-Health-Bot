a5c23f8dd326e9281b9e3652b8cb239a48f26a2b
shubhangi24kashid <kashidshubhangi24@gmail.com>
2023-11-09 13:12:32 +0530

    Added different files for different disorders

---
+++ depression.py
@@ -0,0 +1,8 @@
+import os
+
+def greaterThan3andEualTo4():
+    pass
+
+
+def dangerous():
+    pass
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,3 +1,8 @@
+import os
+import anxiety
+import depression
+import EatingDisorder
+
 def smalltest():
   response=0
   while True:
@@ -10,7 +15,7 @@ def smalltest():
 
   while True:
     res2=int(input("2. Are there any specific situations or events causing you stress or anxiety(Rate in range of 0 to 100%)?"))
-    if res2>0 and res2<100:
+    if res2>0 and res2<10:
       response=response+ res2
       break
     else:
@@ -18,8 +23,8 @@ def smalltest():
 
 
   while True:
-      res3=int(input("3. On a scale of 0 to 100 , how would you rate your overall modd today?"))
-      if res3>0 and res3<100:
+      res3=int(input("3. On a scale of 0 to 10 , how would you rate your overall modd today?"))
+      if res3>0 and res3<10:
         response=response+ res3
         break
       else:
@@ -32,7 +37,195 @@ def smalltest():
 
 #Big test is to identify specific Disorder
 def BigTest():
-  pass
+  print("Now we are going to have diffferent tests for different aspects in mental health")
+  print("Apppearing for anxiety test  (Please answer in (y/n)form ) ")
+  while True:
+      anxious=0
+      res1=str(input("Have you been feeling unusually anxious or worried most days for at least six months?"))
+      if res1.lower() == 'y':
+        anxious+=1
+        break
+      elif res1.lower() == 'n':
+        print("Thank you!!")
+        break
+      else:
+        print(" Please enter the correct answer")  
+  while True:
+      res2=str(input("2.Do you often experience physical symptoms like racing heart, sweating,or trembling when you're anxious? "))
+      if res2.lower() == 'y':
+        anxious+=1
+        break
+      elif res2.lower() == 'n':
+        print("Thank you!!")
+        break
+  while True:
+      res3=str(input("3.Are you avoiding certain situations or activities because of your anxiety?"))
+      if res3.lower() == 'y':
+        anxious+=1
+        break
+      elif res3.lower() == 'n':
+        print("Thank you!!")
+        break
+      else:
+        print(" Please enter the correct answer")    
+  while True:
+      res4=str(input("4. Have you naticed a significant changes in your sleep patterns, such as difficulty falling asleep or staying asleep due to anxiety ?"))
+      if res4.lower() == 'y':
+        anxious+=1
+        break
+      elif res4.lower() == 'n':
+        print("Thank you!!")
+        break    
+      else:
+        print(" Please enter the correct answer")     
+  while True:
+      res5=str(input("5. Do you find it challenging to control your worry or anxious thoughts , even when you try to redirect your focus? "))
+      if res5.lower() == 'y':
+        anxious+=1
+        break
+      elif res5.lower() == 'n':
+        print("Thank you!!")
+        break
+      else:
+        print(" Please enter the correct answer")
+
+  if (anxious>=2 and anxious<=3):
+    print(" We have seen some symptoms in you so are here to give some relevant information to you Please read that carefully ")
+    result=anxiety.moderate()
+    print(result)
+  elif(anxious>=4 and anxious==5):
+    result=anxiety.dangerous() 
+    print(result)
+  else:
+    print("Ohh goood , You don't have such a symptoms regarding anxiety")  
+
+
+  print("Apppearing for Depression test (Please answer in (y/n)form )")
+  while True:
+      deprees=0
+      res1=str(input("1. Have you been persistently feeling sad or down for most of the day, nearly every day, for at least two weeks or longer?"))
+      if res1.lower() == 'y':
+        deprees+=1
+        break
+      elif res1.lower() == 'n':
+        print("Thank you!!")
+        break
+      else:
+        print(" Please enter the correct answer")  
+  while True:
+      res2=str(input("2.Are you experiencing a significant decrease in interest or pleasure in activities that you used to enjoy? "))
+      if res2.lower() == 'y':
+        deprees+=1
+        break
+      elif res2.lower() == 'n':
+        print("Thank you!!")
+        break
+  while True:
+      res3=str(input("3.Have you noticed changes in your sleep patterns, such as trouble falling asleep, staying asleep, or oversleeping?"))
+      if res3.lower() == 'y':
+        deprees+=1
+        break
+      elif res3.lower() == 'n':
+        print("Thank you!!")
+        break
+      else:
+        print(" Please enter the correct answer")    
+  while True:
+      res4=str(input("4.Have there been changes in your appetite or weight, like significant changes in eating habits or weight gain/loss?"))
+      if res4.lower() == 'y':
+        deprees+=1
+        break
+      elif res4.lower() == 'n':
+        print("Thank you!!")  
+        break  
+      else:
+        print(" Please enter the correct answer")     
+  while True:
+      res5=str(input("5. Do you find it challenging to concentrate, make decisions, or have you experienced a decrease in your ability to think clearly? "))
+      if res5.lower() == 'y':
+        deprees+=1
+        break
+      elif res5.lower() == 'n':
+        print("Thank you!!")
+        break
+      else:
+        print(" Please enter the correct answer")
+
+  if (deprees>=2 and deprees<=3):
+    print(" We have seen some symptoms in you so are here to give some relevant information to you Please read that carefully ")
+    result=depression.moderate()
+    print(result)
+  elif(deprees>=4 and deprees==5):
+    result=depression.dangerous() 
+    print(result)
+  else:
+    print("Ohh goood , You don't have such a symptoms regarding Depression") 
+
+
+  print("Apppearing for Eating Disorder test (Please answer in (y/n)form )")
+  while True:
+      Eating=0
+      res1=str(input("1. Have you noticed any significant changes in your eating habits, such as eating much less or much more than usual?"))
+      if res1.lower() == 'y':
+         Eating+=1
+         break
+      elif res1.lower() == 'n':
+        print("Thank you!!")
+        break
+      else:
+        print(" Please enter the correct answer")  
+  while True:
+      res2=str(input("2.Do you often feel a strong need to control your weight or body shape through dieting or excessive exercise?"))
+      if res2.lower() == 'y':
+         Eating+=1
+         break
+      elif res2.lower() == 'n':
+        print("Thank you!!")
+        break
+  while True:
+      res3=str(input("3.Have you experienced any physical or emotional health issues that you suspect might be related to your eating habits?"))
+      if res3.lower() == 'y':
+         Eating+=1
+         break
+      elif res3.lower() == 'n':
+        print("Thank you!!")
+        break
+      else:
+        print(" Please enter the correct answer")    
+  while True:
+      res4=str(input("4.Are you often dissatisfied with your appearance and body image?"))
+      if res4.lower() == 'y':
+         Eating+=1
+         break
+      elif res4.lower() == 'n':
+        print("Thank you!!") 
+        break   
+      else:
+        print(" Please enter the correct answer")     
+  while True:
+      res5=str(input("5. Have you ever made yourself vomit, used laxatives, or engaged in other behaviors to compensate for the amount of food you've eaten? "))
+      if res5.lower() == 'y':
+         Eating+=1
+         break
+      elif res5.lower() == 'n':
+        print("Thank you!!")
+        break
+      else:
+        print(" Please enter the correct answer")
+
+  if ( Eating>=2 and Eating<=3):
+    print(" We have seen some symptoms in you so are here to give some relevant information to you Please read that carefully ")
+    result=EatingDisorder.moderate()
+    print(result)
+  elif( Eating>=4 and  Eating==5):
+    result=EatingDisorder.dangerous() 
+    print(result)
+  else:
+    print("Ohh goood , You don't have such a symptoms regarding Eating Dosorder")   
+
+  
+
+
 
 #end part regarding awreness
 def endPart():
@@ -46,3 +239,6 @@ if user.lower() == 'y':
 elif user.lower() == 'n':
     print("Thank you!!")
     endPart()
+
+
+
