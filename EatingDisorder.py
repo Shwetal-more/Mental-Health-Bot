435eb87d87f79e800aff70b7790968ee68012741
shubhangi24kashid <kashidshubhangi24@gmail.com>
2023-11-11 01:18:21 +0530

    Added summery for personality test

--- EatingDisorder.py
+++ EatingDisorder.py
@@ -2,26 +2,11 @@
 from transformers import pipeline
 
 def moderate():
-    question_answering = pipeline("question-answering")
-    context = ""
-    question = input("user: ")
-    while True:
-       result = question_answering(question=question, context=context)
-       print("Answer:", result['answer'])
-       break
-    if (question=='Exit' and question=='exit'):
-           return
+    print("Definition:\nDisruptive eating patterns leading to physical and mental health issues.\nIncludes conditions like anorexia nervosa, bulimia nervosa, and binge-eating disorder.\n\nSymptoms:\nAnorexia Nervosa: Extreme calorie restriction, fear of gaining weight.\nBulimia Nervosa: Binge-eating followed by purging behaviors.\nBinge-Eating Disorder: Consuming large amounts of food rapidly, loss of control.\n\nTreatment:\nTherapy: Cognitive-behavioral therapy (CBT), dialectical behavior therapy (DBT).\nNutritional Counseling: Guidance on healthy eating habits.\nMedical Monitoring: Especially for severe cases requiring hospitalization.\n\nSelf-Help at Different Levels:\nMild Eating Disorder: Maintain regular, balanced meals.\nModerate Eating Disorder: Establish a support network, consider self-help resources.\nSevere Eating Disorder: Seek professional help immediately, involve family or friends.\n\nWeekend Activities for Relief:\nRelaxation activities like reading or listening to music.\nEngage in non-food-related social events.\nOutdoor activities promoting a positive body-mind connection.\n\nDiet Recommendations:\nWork with a nutritionist for a balanced, sustainable meal plan.\nEmphasize a variety of food groups.\nAvoid extreme diets and restrictive eating habits.\n\nConclusion:\nAddressing eating disorders requires a comprehensive approach involving therapy, nutritional support, and medical monitoring. Early intervention is crucial, and individuals should seek professional help to develop a personalized treatment plan.")
 
 
 def dangerous():
-    question_answering = pipeline("question-answering")
-    context = "" 
-    question = input("user: ")
-    while True:
-       result = question_answering(question=question, context=context)
-       print("Answer:", result['answer'])
-       break
-    if (question=='Exit' and question=='exit'):
-           return
+    print("Definition:\nDisruptive eating patterns leading to physical and mental health issues.\nIncludes conditions like anorexia nervosa, bulimia nervosa, and binge-eating disorder.\n\nSymptoms:\nAnorexia Nervosa: Extreme calorie restriction, fear of gaining weight.\nBulimia Nervosa: Binge-eating followed by purging behaviors.\nBinge-Eating Disorder: Consuming large amounts of food rapidly, loss of control.\n\nTreatment:\nTherapy: Cognitive-behavioral therapy (CBT), dialectical behavior therapy (DBT).\nNutritional Counseling: Guidance on healthy eating habits.\nMedical Monitoring: Especially for severe cases requiring hospitalization.\n\nSelf-Help at Different Levels:\nMild Eating Disorder: Maintain regular, balanced meals.\nModerate Eating Disorder: Establish a support network, consider self-help resources.\nSevere Eating Disorder: Seek professional help immediately, involve family or friends.\n\nWeekend Activities for Relief:\nRelaxation activities like reading or listening to music.\nEngage in non-food-related social events.\nOutdoor activities promoting a positive body-mind connection.\n\nDiet Recommendations:\nWork with a nutritionist for a balanced, sustainable meal plan.\nEmphasize a variety of food groups.\nAvoid extreme diets and restrictive eating habits.\n\nConclusion:\nAddressing eating disorders requires a comprehensive approach involving therapy, nutritional support, and medical monitoring. Early intervention is crucial, and individuals should seek professional help to develop a personalized treatment plan.")
 
-       
+
+
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,2 +1,17 @@
-print("Anxiety Overview: Definition:  Normal emotion of worry, fear, or unease.\n Becomes a concern when excessive, persistent, and disruptive (anxiety disorders).\n\n Symptoms:Physical: Rapid heartbeat, muscle tension, sweating.Cognitive: Excessive worrying, racing thoughts.\n Emotional: Restlessness, irritability, trouble sleeping.\n\nTreatment:\n\Therapy: Cognitive-behavioral therapy (CBT), exposure therapy.\n\Medications: Antidepressants, benzodiazepines (short-term), beta-blockers.\n\Lifestyle Changes: Exercise, healthy diet, adequate sleep, stress management.\n\nSelf-Help at Different Levels:\nMild Anxiety: Deep breathing, challenging irrational thoughts.\nModerate Anxiety: Journaling, physical activities.\nSevere Anxiety: Seek professional help, consider medication.\n\nWeekend Activities for Relief:Nature walks, creative outlets, socializing, relaxation techniques.\n\nDiet Recommendations:\nOmega-3 fatty acids (fish, flaxseeds, walnuts).\nWhole grains for steady energy.\nFruits and vegetables for antioxidants and vitamins.\nLimit caffeine and sugar intake.\n\nConclusion:\nIndividual responses vary, and consulting a healthcare professional for personalized advice is crucial. Implementing a holistic approach involving therapy, medication (if necessary), and lifestyle changes can contribute to managing anxiety effectively.")    
+from transformers import pipeline
 
+#def moderate():
+question_answering = pipeline("question-answering")
+context = "what is hyperactivity?1 Hyperactivity means a person may seem to move about constantly including in situations when it is not appropriate or excessively fidgets taps or talks In adults hyperactivity may mean extreme restlessness or talking too much.How to reduce hyperactivity?1 Give clear effective directions or commands Establish healthy habits 2 Develop routines around homework and chores 3 Help your child build relationships strong social skills and maintain friendships.What is an example of hyperactivity disorder?The main signs of hyperactivity and impulsiveness are :1  being unable to sit still especially in calm or quiet surroundings constantly fidgeting 2 being unable to concentrate on tasks.Is hyperactive child normal?Most healthy children are inattentive, hyperactive or impulsive at one time or another.Can hyperactive go away with age? People may find that their symptoms lessen and increase at times throughout their life.Is hyperactive good or bad?Hyperactivity is often considered more of a problem for schools and parents than it is for the child.Is hyperactivity serious? The symptoms of ADHD can interfere significantly with an individual's daily activities and relationships. What foods decrease hyperactivity?A high-protein diet like Beans, cheese, eggs, meat, and nuts can be good sources of protein.Is hyperactivity Treatable?Standard treatments for ADHD in adults typically involve medication, education, skills training and psychological counseling.Is hyperactive a mental disorder?Attention-deficit/hyperactivity disorder (ADHD) is one of the most common mental disorders affecting children."
+
+question = input("user: ")
+i=0
+while i<=5:
+   result = question_answering(question=question, context=context)
+   print("Answer:", result['answer'])
+   i+=1
+   break
+if (question=='Exit' and question=='exit'):
+    print("Summary")
+  
+   
